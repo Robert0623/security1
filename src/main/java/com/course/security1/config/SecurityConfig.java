44f7a9ca443d7dd74d186188bafd0f04b5e4e673
hwoo <tone8943@gmail.com>
2025-05-18 16:35:16 +0900

    시큐리티 설정

---
+++ src/main/java/com/course/security1/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.course.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/**").authenticated() // 인증(로그인) 필요
+                        .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // 인증과 role 필요
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // 인증과 role 필요
+                        .anyRequest().permitAll()) // 나머지 인증(로그인) 필요 X
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form ->
+                        form.loginPage("/login"))
+                .build();
+    }
+
+}
--- src/main/java/com/course/security1/controller/IndexController.java
+++ src/main/java/com/course/security1/controller/IndexController.java
@@ -1,10 +1,10 @@
 package com.course.security1.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
     @GetMapping({"", "/"})
@@ -12,5 +12,37 @@ public class IndexController {
         // mustache 기본 경로 src/main/resources/
         // view resolver 설정 --> templates (prefix), .mustache (suffix) 생략 가능
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 낚아챔
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    @ResponseBody
+    public String joinProc() {
+        return "회원가입 완료됨";
     }
 }
