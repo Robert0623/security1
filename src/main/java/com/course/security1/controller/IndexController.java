fea49207182dd934c6e997394cdb937b63eb2154
hwoo <tone8943@gmail.com>
2025-05-18 16:03:34 +0900

    mustache 설정

--- src/main/java/com/course/security1/controller/IndexController.java
+++ src/main/java/com/course/security1/controller/IndexController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping({"", "/"})
     public String index() {
         // mustache 기본 경로 src/main/resources/
-        // view resolver 설정 /templates/
+        // view resolver 설정 --> templates (prefix), .mustache (suffix) 생략 가능
         return "index";
     }
 }
